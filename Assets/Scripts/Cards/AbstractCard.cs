3f0b24ec8ef856366e0180771108b78ae444c634
jindastrialman <keklolich@gmail.com>
2021-10-24 11:28:20 +0300

    Абстракции для карт

---
+++ Assets/Scripts/Cards/AbstractCard.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class AbstractCard
+{
+    private int _manacost;
+    private AbstractCastingType _type;
+    private AbstractEffect _effect;
+}
---
+++ Assets/Scripts/Player/AbstractPlayer.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AbstractPlayer
+{
+    private List<AbstractCard> _hand;
+    private List<AbstractCard> _frontline;
+    private List<AbstractCard> _backline;
+}
